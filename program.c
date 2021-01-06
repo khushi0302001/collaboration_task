8756a9867462af25b34f1f5184beb6d8c0a98654
Harsh Gupta <harshguptaucme@gmail.com>
2021-01-06 17:51:21 +0530

    created branch add-feature and made some changes

--- program.c
+++ program.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 #include<math.h>
 
+int sum(int ,int);
+
 int main(){
 	printf("hello funccforce members");
+	
+	printf("The sum of these numbers :%d",sum(10,10));
 }
 
-
+int sum(int a, int b)//defining function based in declaration
+{
+    int result=a+b;//find sum of two numbers
+    //and result of sum stored in result variable
+    return result;//returning result
+}
 	
